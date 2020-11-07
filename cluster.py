bdc395532a707a3dad544f0d396b45e69049b73e
mfknie <mfknie@uchicago.edu>
2020-11-06 19:20:17 -0500

    Added some basic db processing scripts

--- cluster.py
+++ cluster.py
@@ -1,5 +1,7 @@
+# %%
 import pandas as pd
 import numpy as np
+# %%
 from sklearn.cluster import DBSCAN, KMeans, AgglomerativeClustering
 from sklearn.decomposiiton import PCA
 from sklearn.preprocessing import StandardScaler
@@ -11,68 +13,7 @@ def import_data(file):
     df=pd.read_csv(file, header = 0)
     return df.to_numpy()
 
-
-def kmeans_clusters(X, n_clusters = None):
-    # Adapted from:
-    # https://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_analysis.html#sphx-glr-auto-examples-cluster-plot-kmeans-silhouette-analysis-py
-    if(not n_clusters):
-        n_clusters = [5, 10, 14, 15, 16]
-    results = cluster.cv_results_
-    data = pd.DataFrame(results)
-    print(data[["params", "mean_train_score", "mean_test_score"]])
-    for size in n_clusters:
-        # Create a subplot with 1 row and 2 columns
-        fig, ax1 = plt.subplots(1)
-        fig.set_size_inches(18, 7)
-
-        ax1.set_xlim([-0.1, 1])
-        ax1.set_ylim([0, len(X) + (sizes + 1) * 10])
-
-        kmeans = KMeans(n_clusters=n_clusters, random_state=42)
-        labels = clusterer.fit_predict(X)
-
-        silhouette_avg = silhouette_score(X, cluster_labels)
-        print("For ", n_clusters,
-            " clusters, the average silhouette_score is :", silhouette_avg)
-
-        sample_silhouette_values = silhouette_samples(X, cluster_labels)
-
-        y_lower = 10
-        for i in range(size):
-            ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i].sort()
-            size_cluster_i = ith_cluster_silhouette_values.shape[0]
-            y_upper = y_lower + size_cluster_i
-
-            color = cm.nipy_spectral(float(i) / n_clusters)
-            ax1.fill_betweenx(np.arange(y_lower, y_upper),
-                            0, ith_cluster_silhouette_values,
-                            facecolor=color, edgecolor=color, alpha=0.7)
-
-            # Label the silhouette plots with their cluster numbers at the middle
-            ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
-            y_lower = y_upper + 10  
-        
-        ax1.set_title("The silhouette plot for the various clusters.")
-        ax1.set_xlabel("The silhouette coefficient values")
-        ax1.set_ylabel("Cluster labels")
-
-        # The vertical line for average silhouette score of all the values
-        ax1.axvline(x=silhouette_avg, color="red", linestyle="b-")
-
-        ax1.set_yticks([])  # Clear the yaxis labels / ticks
-        ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
-
-        plt.suptitle(("KMeans on sample data "
-                    "with n_clusters = {:d}".format(n_clusters)),
-                    fontsize=20, fontweight='bold')
-        
-# Basic PCA, data processing stuff here
-def find_pcs(data):
-    scaled_data = StandardScaler().fit_transform(data)
-    pca = PCA(n_components=0.95)
-    pca.fit(data)
-    components = pd.DataFrame(pca.components_, columns = cols) 
-    components["var_explained"] = pca.explained_variance_ratio_
-    return components
+def run_kmeans():
     
+        
 if __name__ == "__main__":
--- mySQL.py
+++ mySQL.py
@@ -1,25 +1,55 @@
+# %%
 import pandas as pd
 import numpy as np
 import mysql.connector
-from cluster import app
+import sqlalchemy
+from config import App
+# %%
 
 def import_data(file):
     pd.read_csv(file)
 
-def write_data():
+def write_data(df, name):
+    df.to_csv("data/"+name+".csv", index=False)
 
-    df.to_csv()
+def write_data_incr(df):
+    df.to_csv("data/"+name+".csv", index=False, mode="a")
 
-def write_data_incr():
-    df.to_csv(mode="a")
-
+# %%
 if __name__ == "__main__":
-    with connect = mysql.connector.connect(user=App.config("username")
-            password=App.config("password")
-            host=""
-            database=App.config("MYSQL_DB")
-        ):
-        query = ("")
-        cursor = connect.cursor()
-        cursor.execute()
-        for row in query:
+    # %%
+    connect_str = "mysql+mysqlconnector://{0}:{1}@{2}/{3}".format(
+        App.config("username"), 
+        App.config("password"), 
+        "localhost",
+        App.config("MYSQL_DB"))
+    engine = sqlalchemy.create_engine(connect_str)
+    
+    # %%
+    chr_genre_query = ("""select characters.id, characters.fullname, genres.genre 
+        from characters inner join genres on characters.genres LIKE genres.id;
+    """)
+    movie_genre_query = ("""select characters.id, characters.fullname, title_movie_genres.genre from characters 
+        inner join title_movie_genres_pivot on characters.bookmovieid = title_movie_genres_pivot.title_id
+        inner join title_movie_genres on title_movie_genres_pivot.genre_id = title_movie_genres.id;
+    """)
+    user_traits_query = ("""select * from usertraits""")
+    user_likes_query = ("""select * from likeable_likes where likable_type = "character" """)
+    
+# %%
+    genre_df = None
+# %%
+    conn = engine.connect()
+# %%
+    with engine.connect() as conn:
+# %%    
+        write_data(pd.read_sql(sql = chr_genre_query, con = conn), "char_genre")
+        write_data(pd.read_sql(sql = movie_genre_query, con = conn), "movie_genres")
+        write_data(pd.read_sql(sql = user_traits_query, con = conn), "user_traits")
+        write_data(pd.read_sql(sql = user_likes_query, con = conn), "user_likes_chars")
+
+# %%
+# for VSscode jupyter
+    conn.close()
+
+# %%
